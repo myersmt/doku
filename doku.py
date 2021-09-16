6a22732607b4868f62c61e812af9d977f3ecd0a4
Matt Myers <myersmt@pm.me>
2021-09-16 12:47:58 -0400

    init

---
+++ doku.py
@@ -0,0 +1,11 @@
+'''
+Matt Myers
+09/16/2021
+Sudoku Game and Solver
+Sudoku is a classic intellectual game where there is a 3x3
+grid with a 3x3 matrix inside making 9 large zones with 81
+total squares. The goal of this game is to complete all 81
+squares following the set rules. The rules are in each 
+large square it must go 1-9 without repeating and in each
+column and row it must go 1-9 without repeating.
+'''
